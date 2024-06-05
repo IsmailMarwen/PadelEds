0ed24ef0f146e3392514469ec1bf1b1efa61c2b7
IsmailMarwen <ismail.marwen.ismail@gmail.com>
2024-06-05 12:21:37 +0100

    refactor name

--- EDS_Padel/src/main/java/com/example/demo/EdsPadelApplication.java
+++ EDS_Padel/src/main/java/com/example/demo/EdsPadelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RendezvousApplication {
+public class EdsPadelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RendezvousApplication.class, args);
+		SpringApplication.run(EdsPadelApplication.class, args);
 	}
 
 }
