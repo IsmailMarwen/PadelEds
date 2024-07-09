a3947e42be5e82d1f3cbbda206c1a1425fe1f30e
IsmailMarwen <ismail.marwen.ismail@gmail.com>
2024-07-09 10:31:07 +0100

    resolve problem

--- EDS_Padel/src/main/java/com/example/demo/service/controller/NotificationController.java
+++ EDS_Padel/src/main/java/com/example/demo/service/controller/NotificationController.java
@@ -1,4 +1,27 @@
 package com.example.demo.service.controller;
 
+import com.example.demo.persistance.entities.Administrateur;
+import com.example.demo.persistance.entities.Notification;
+import com.example.demo.service.impliments.AdministrateurService;
+import com.example.demo.service.impliments.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notifications")
 public class NotificationController {
+    @Autowired
+    private NotificationService notificationService;
+    @Autowired
+    private AdministrateurService administrateurService;
+    @GetMapping("/admin/{adminId}")
+    public List<Notification> getNotificationsForAdmin(@PathVariable Long adminId) {
+        Administrateur admin = administrateurService.getAdminstarteurByIdAdminstarteur(adminId); // Assuming you have this method in your service
+        return notificationService.getNotificationsForAdmin(admin);
+    }
 }
