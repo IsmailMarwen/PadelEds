822d90c39bc0d6812e92f4c2707747152caf3a4c
IsmailMarwen <85138402+IsmailMarwen@users.noreply.github.com>
2024-07-12 10:46:38 +0100

    Update Administrateur.java

--- EDS_Padel/src/main/java/com/example/demo/persistance/entities/Administrateur.java
+++ EDS_Padel/src/main/java/com/example/demo/persistance/entities/Administrateur.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 public class Administrateur extends Utilisateur {
-    @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Notification> notifications;
 }
