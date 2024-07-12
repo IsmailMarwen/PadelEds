534006e91d2254cc65f59f2cf7ae484c7137bdc5
IsmailMarwen <ismail.marwen.ismail@gmail.com>
2024-07-12 10:53:36 +0100

    errors

--- EDS_Padel/src/main/java/com/example/demo/persistance/entities/AgentAcceuil.java
+++ EDS_Padel/src/main/java/com/example/demo/persistance/entities/AgentAcceuil.java
@@ -2,6 +2,7 @@ package com.example.demo.persistance.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
--- EDS_Padel/src/main/java/com/example/demo/persistance/entities/Coach.java
+++ EDS_Padel/src/main/java/com/example/demo/persistance/entities/Coach.java
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.persistence.CascadeType;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
