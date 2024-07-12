34ff5c88a7118c462426ee34e88013dc7cdcee7e
IsmailMarwen <85138402+IsmailMarwen@users.noreply.github.com>
2024-07-12 10:47:07 +0100

    Update AgentAcceuil.java

--- EDS_Padel/src/main/java/com/example/demo/persistance/entities/AgentAcceuil.java
+++ EDS_Padel/src/main/java/com/example/demo/persistance/entities/AgentAcceuil.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Entity
 
 public class AgentAcceuil extends Utilisateur{
-    @OneToMany(mappedBy = "agentAcceuil", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "agentAcceuil", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Notification> notifications;
 }
