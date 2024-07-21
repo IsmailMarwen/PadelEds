de4ead890c07532c227907aca66e080c7b5b0931
hamden-smaoui <116950925+hamden-smaoui@users.noreply.github.com>
2024-07-21 13:39:21 +0100

    Update Activite.java

--- EDS_Padel/src/main/java/com/example/demo/persistance/entities/Activite.java
+++ EDS_Padel/src/main/java/com/example/demo/persistance/entities/Activite.java
@@ -24,6 +24,7 @@ public class Activite {
     private List<CategorieAbonnement> categorieAbonnements;
 
     @OneToMany(mappedBy="activite",fetch=FetchType.LAZY)
+    @JsonIgnore
     private List<Ressource> ressources;
    
 
